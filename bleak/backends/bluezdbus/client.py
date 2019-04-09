d0fbd758c49fbd3083609f0736d06c7b86ece5d4
Henrik Blidh <henrik.blidh@swedwise.se>
2019-04-09 22:34:23 +0200

    BlueZ Battery Level Notifications handling
    Improvement for #55
    Wrote better error for battery notification in BlueZ >= 5.48

--- bleak/backends/bluezdbus/client.py
+++ bleak/backends/bluezdbus/client.py
@@ -362,9 +362,24 @@ class BleakClientBlueZDBus(BaseBleakClient):
                 notification to bytearray.
 
         """
-
         _wrap = kwargs.get("notification_wrapper", True)
         characteristic = self.services.get_characteristic(str(_uuid))
+        if not characteristic:
+            # Special handling for BlueZ >= 5.48, where Battery Service (0000180f-0000-1000-8000-00805f9b34fb:)
+            # has been moved to interface org.bluez.Battery1 instead of as a regular service.
+            # The org.bluez.Battery1 on the other hand does not provide a notification method, so here we cannot
+            # provide this functionality...
+            # See https://kernel.googlesource.com/pub/scm/bluetooth/bluez/+/refs/tags/5.48/doc/battery-api.txt
+            if _uuid == "00002a19-0000-1000-8000-00805f9b34fb" and (
+                self._bluez_version[0] == 5 and self._bluez_version[1] >= 48
+            ):
+                raise BleakError(
+                    "Notifications on Battery Level Char ({0}) is not "
+                    "possible in BlueZ >= 5.48. Use regular read instead.".format(_uuid)
+                )
+            raise BleakError(
+                "Characteristic with UUID {0} could not be found!".format(_uuid)
+            )
         await self._bus.callRemote(
             characteristic.path,
             "StartNotify",
