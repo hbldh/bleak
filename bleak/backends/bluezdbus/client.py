ad2507bf1426d0e5dc59f7457a4dccaa523c5981
Henrik Blidh <henrik.blidh@swedwise.com>
2019-05-25 19:53:37 +0200

    Refactored DBus backend services resolver somewhat Merge #66

--- bleak/backends/bluezdbus/client.py
+++ bleak/backends/bluezdbus/client.py
@@ -176,8 +176,11 @@ class BleakClientBlueZDBus(BaseBleakClient):
             self._bus, self.loop, self._device_path + "/service"
         )
 
-        # no guarantee service listed before charachteristics in objs
-        # need multiple iterations
+        # There is no guarantee that services are listed before characteristics
+        # Managed Objects dict.
+        # Need multiple iterations to construct the Service Collection
+
+        _chars, _descs = [], []
 
         for object_path, interfaces in objs.items():
             logger.debug(utils.format_GATT_object(object_path, interfaces))
@@ -186,36 +189,36 @@ class BleakClientBlueZDBus(BaseBleakClient):
                 self.services.add_service(
                     BleakGATTServiceBlueZDBus(service, object_path)
                 )
-
-        for object_path, interfaces in objs.items():
-            logger.debug(utils.format_GATT_object(object_path, interfaces))
-            if defs.GATT_CHARACTERISTIC_INTERFACE in interfaces:
+            elif defs.GATT_CHARACTERISTIC_INTERFACE in interfaces:
                 char = interfaces.get(defs.GATT_CHARACTERISTIC_INTERFACE)
-                _service = list(
-                    filter(lambda x: x.path == char["Service"], self.services)
-                )
-                self.services.add_characteristic(
-                    BleakGATTCharacteristicBlueZDBus(
-                        char, object_path, _service[0].uuid
-                    )
-                )
-                self._char_path_to_uuid[object_path] = char.get("UUID")
-
-        for object_path, interfaces in objs.items():
-            logger.debug(utils.format_GATT_object(object_path, interfaces))
-            if defs.GATT_DESCRIPTOR_INTERFACE in interfaces:
+                _chars.append([char, object_path])
+            elif defs.GATT_DESCRIPTOR_INTERFACE in interfaces:
                 desc = interfaces.get(defs.GATT_DESCRIPTOR_INTERFACE)
-                _characteristic = list(
-                    filter(
-                        lambda x: x.path == desc["Characteristic"],
-                        self.services.characteristics.values(),
-                    )
+                _descs.append([desc, object_path])
+
+        for char, object_path in _chars:
+            _service = list(
+                filter(lambda x: x.path == char["Service"], self.services)
+            )
+            self.services.add_characteristic(
+                BleakGATTCharacteristicBlueZDBus(
+                    char, object_path, _service[0].uuid
                 )
-                self.services.add_descriptor(
-                    BleakGATTDescriptorBlueZDBus(
-                        desc, object_path, _characteristic[0].uuid
-                    )
+            )
+            self._char_path_to_uuid[object_path] = char.get("UUID")
+
+        for desc, object_path in _descs:
+            _characteristic = list(
+                filter(
+                    lambda x: x.path == desc["Characteristic"],
+                    self.services.characteristics.values(),
                 )
+            )
+            self.services.add_descriptor(
+                BleakGATTDescriptorBlueZDBus(
+                    desc, object_path, _characteristic[0].uuid
+                )
+            )
 
         self._services_resolved = True
         return self.services
