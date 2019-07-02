fa4a803c8ee520dbee55568b0f41ab89e8931b73
Kevin Davis <kevincarrolldavis@gmail.com>
2019-07-01 21:31:54 -0400

    added discover functions and read characteristic function

--- bleak/backends/corebluetooth/PeripheralDelegate.py
+++ bleak/backends/corebluetooth/PeripheralDelegate.py
@@ -16,6 +16,9 @@ from bleak.backends.corebluetooth.corebleak import CoreBleak
 from Foundation import NSObject, \
         CBPeripheral, \
         CBService, \
+        CBCharacteristic, \
+        CBDescriptor, \
+        NSData, \
         NSError
 
 logging.basicConfig(level=logging.DEBUG)
@@ -38,6 +41,10 @@ class PeripheralDelegate(NSObject):
         self.peripheral = peripheral
         CoreBleak.assignPeripheralDelegate_toPeripheral_(self, self.peripheral)
         self._services_discovered = False
+        self._service_characteristic_discovered_log = {}
+        self._characteristic_descriptor_log = {}
+
+        self._characteristic_value_log = {}
 
         if not self.compliant():
             logger.warning("PeripheralDelegate is not compliant")
@@ -50,19 +57,85 @@ class PeripheralDelegate(NSObject):
 
     async def discoverServices(self) -> [CBService]:
         if self._services_discovered:
-            return self.peripheral.services
+            return self.peripheral.services()
 
-        self.peripheral.delegate().peripheral.discoverServices_(None)
+        self.peripheral.discoverServices_(None)
 
         while not self._services_discovered:
-            await asyncio.sleep(0.05)
+            await asyncio.sleep(0.01)
 
         return self.peripheral.services()
 
+    async def discoverCharacteristics_(self, service: CBService) -> [CBCharacteristic]:
+        if service.characteristics() is not None:
+            return service.characteristics()
+
+        serviceUUID = service.UUID().UUIDString()
+        self._service_characteristic_discovered_log[serviceUUID] = False
+
+        self.peripheral.discoverCharacteristics_forService_(None, service)
+
+        while not self._service_characteristic_discovered_log[serviceUUID]:
+            await asyncio.sleep(0.01)
+
+        return service.characteristics()
+
+    async def discoverDescriptors_(self, characteristic: CBCharacteristic) -> [CBDescriptor]:
+        if characteristic.descriptors() is not None:
+            return characteristic.descriptors()
+        
+        cUUID = characteristic.UUID().UUIDString()
+        self._characteristic_descriptor_log[cUUID] = False
+
+        self.peripheral.discoverDescriptorsForCharacteristic_(characteristic)
+
+        while not self._characteristic_descriptor_log[cUUID]:
+            await asyncio.sleep(0.01)
+
+        return characteristic.descriptors()
+
+    async def readCharacteristic_(self, characteristic: CBCharacteristic) -> NSData:
+        if characteristic.value() is not None:
+            return characteristic.value()
+
+        cUUID = characteristic.UUID().UUIDString()
+        self._characteristic_value_log[cUUID] = False
+
+        self.peripheral.readValueForCharacteristic_(characteristic)
+
+        while not self._characteristic_value_log[cUUID]:
+            await asyncio.sleep(0.01)
+
+        return characteristic.value()
+
     # Protocol Functions
     def peripheral_didDiscoverServices_(self, peripheral: CBPeripheral, error: NSError) -> None:
-        if error != None:
+        if error is not None:
             raise BleakError(f"Failed to discover services {error}")
 
         logger.debug("Serivces Discovered")
         self._services_discovered = True
+
+    def peripheral_didDiscoverCharacteristicsForService_error_(self, peripheral: CBPeripheral, service:CBService, error: NSError):
+        serviceUUID = service.UUID().UUIDString()
+        if error is not None:
+            raise BleakError(f"Failed to discover services for service {serviceUUID}: {error}")
+
+        logger.debug("Characteristics discovrered")
+        self._service_characteristic_discovered_log[serviceUUID] = True
+
+    def peripheral_didDiscoverDescriptorsForCharacteristic_error_(self, peripheral: CBPeripheral, characteristic: CBCharacteristic, error: NSError):
+        cUUID = characteristic.UUID().UUIDString()
+        if error is not None:
+            raise BleakError(f"Failed to discover descriptors for characteristic {cUUID}: {error}")
+
+        logger.debug("Descriptor discovered")
+        self._characteristic_descriptor_log[cUUID] = True
+
+    def peripheral_didUpdateValueForCharacteristic_error_(self, peripheral: CBPeripheral, characteristic: CBCharacteristic, error: NSError):
+        cUUID = characteristic.UUID().UUIDString()
+        if error is not None:
+            raise BleakError(f"Failed to read characteristic {cUUID}: {error}")
+
+        logger.debug("Read characteristic value")
+        self._characteristic_value_log[cUUID] = True
