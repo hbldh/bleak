8de1b01cbe5c2cd83b58f528eb9f7561d381964f
Bill Siever <bsiever@gmail.com>
2020-05-03 12:30:21 -0500

    Updated scan callback (unused ret value)

--- bleak/backends/corebluetooth/CentralManagerDelegate.py
+++ bleak/backends/corebluetooth/CentralManagerDelegate.py
@@ -1,6 +1,6 @@
 """
 CentralManagerDelegate will implement the CBCentralManagerDelegate protocol to
-manage CoreBluetooth serivces and resources on the Central End
+manage CoreBluetooth services and resources on the Central End
 
 Created on June, 25 2019 by kevincar <kevincarrolldavis@gmail.com>
 
@@ -8,6 +8,7 @@ Created on June, 25 2019 by kevincar <kevincarrolldavis@gmail.com>
 
 import asyncio
 import logging
+import weakref
 from enum import Enum
 from typing import List
 
@@ -57,7 +58,12 @@ class CentralManagerDelegate(NSObject):
         self.connected_peripheral_delegate = None
         self.connected_peripheral = None
         self._connection_state = CMDConnectionState.DISCONNECTED
-
+        
+        
+        # TODO:  Weak references to Client objects (and on disconnect, lookup and try to call disconnect)
+        #  Map of Address -> (WeakRef) to client
+        #  weakref.WeakValueDictionary
+        self._disconnected_callback = {}    # Map of Address to callback
         self.ready = False
         self.devices = {}
 
@@ -91,7 +97,7 @@ class CentralManagerDelegate(NSObject):
             await asyncio.sleep(0)
         return self.ready
 
-    async def scanForPeripherals_(self, scan_options) -> List[CBPeripheral]:
+    async def scanForPeripherals_(self, scan_options):
         """
         Scan for peripheral devices
         scan_options = { service_uuids, timeout }
@@ -120,7 +126,6 @@ class CentralManagerDelegate(NSObject):
         while self.central_manager.isScanning():
             await asyncio.sleep(0.1)
 
-        return []
 
     async def connect_(self, peripheral: CBPeripheral) -> bool:
         self._connection_state = CMDConnectionState.PENDING
@@ -219,9 +224,13 @@ class CentralManagerDelegate(NSObject):
     def centralManager_didDisconnectPeripheral_error_(
         self, central: CBCentralManager, peripheral: CBPeripheral, error: NSError
     ):
+        print("Disconnect********")
         logger.debug("Peripheral Device disconnected!")
         self._connection_state = CMDConnectionState.DISCONNECTED
-
+        # See if there's a callback in the dictionary!
+        address = peripheral.identifier().UUIDString()
+        if address in self._disconnected_callback:
+            self._disconnected_callback[address]()
 
 def string2uuid(uuid_str: str) -> CBUUID:
     """Convert a string to a uuid"""
