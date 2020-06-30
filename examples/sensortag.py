91bcacc6a9f08fbadaad6c868706af6f7bff35f5
Henrik Blidh <henrik.blidh@swedwise.se>
2020-06-30 13:11:07 +0200

    Changed macOS example uuids

--- examples/sensortag.py
+++ examples/sensortag.py
@@ -164,7 +164,7 @@ if __name__ == "__main__":
     address = (
         "24:71:89:cc:09:05"
         if platform.system() != "Darwin"
-        else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"
+        else "B9EA5233-37EF-4DD6-87A8-2A875E821C46"
     )
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(address, loop, True))
