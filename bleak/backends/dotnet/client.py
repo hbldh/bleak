0ea60ee871770c89465466cf6155916e30032204
Henrik Blidh <henrik.blidh@swedwise.com>
2021-01-11 14:44:07 +0100

    Fix for pairing in .NET backend.

--- bleak/backends/dotnet/client.py
+++ bleak/backends/dotnet/client.py
@@ -43,6 +43,9 @@ from Windows.Devices.Enumeration import (
     DeviceUnpairingResult,
     DeviceUnpairingResultStatus,
     DevicePairingKinds,
+    DevicePairingProtectionLevel,
+    DeviceInformationCustomPairing,
+    DevicePairingRequestedEventArgs,
 )
 from Windows.Devices.Bluetooth import (
     BluetoothLEDevice,
@@ -136,8 +139,8 @@ class BleakClientDotNet(BaseBleakClient):
 
         """
         # Try to find the desired device.
+        timeout = kwargs.get("timeout", self._timeout)
         if self._device_info is None:
-            timeout = kwargs.get("timeout", self._timeout)
             device = await BleakScannerDotNet.find_device_by_address(
                 self.address, timeout=timeout
             )
@@ -228,7 +231,7 @@ class BleakClientDotNet(BaseBleakClient):
             # This keeps the device connected until we dispose the session or
             # until we set MaintainConnection = False.
             self._session.MaintainConnection = True
-            await asyncio.wait_for(event.wait(), timeout=10)
+            await asyncio.wait_for(event.wait(), timeout=timeout)
         except BaseException:
             handle_disconnect()
             raise
@@ -319,7 +322,9 @@ class BleakClientDotNet(BaseBleakClient):
             def handler(sender, args):
                 args.Accept()
 
-            custom_pairing.PairingRequested += handler
+            pairing_requested_token = custom_pairing.add_PairingRequested(
+                TypedEventHandler[DeviceInformationCustomPairing, DevicePairingRequestedEventArgs](handler)
+            )
 
             if protection_level:
                 raise NotImplementedError(
@@ -333,13 +338,7 @@ class BleakClientDotNet(BaseBleakClient):
                     return_type=DevicePairingResult,
                 )
 
-            try:
-                custom_pairing.PairingRequested -= handler
-            except Exception:
-                # TODO: Find a way to remove WinRT events...
-                pass
-            finally:
-                del handler
+            custom_pairing.remove_PairingRequested(pairing_requested_token)
 
             if pairing_result.Status not in (
                 DevicePairingResultStatus.Paired,
@@ -357,11 +356,14 @@ class BleakClientDotNet(BaseBleakClient):
                         pairing_result.ProtectionLevelUsed
                     )
                 )
-
-        return self._requester.DeviceInformation.Pairing.IsPaired
+                return True
+        else:
+            return self._requester.DeviceInformation.Pairing.IsPaired
 
     async def unpair(self) -> bool:
         """Attempts to unpair from the device.
+
+        N.B. unpairing also leads to disconnection in the Windows backend.
 
         Returns:
             Boolean on whether the unparing was successful.
@@ -388,6 +390,7 @@ class BleakClientDotNet(BaseBleakClient):
                 )
             else:
                 logger.info("Unpaired with device.")
+                return True
 
         return not self._requester.DeviceInformation.Pairing.IsPaired
 
