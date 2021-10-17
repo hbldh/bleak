48b79a4a45c0072e8e9ebd90ba70e801bc41fff9
David Lechner <david@pybricks.com>
2021-10-17 15:51:11 -0500

    p4a: remove CONTROLLER_ERROR_CODES from error code table
    The values in CONTROLLER_ERROR_CODES overlap with PROTOCOL_ERROR_CODES
    so we will need a separate lookup table if these error codes are
    actually needed.

--- bleak/backends/p4android/defs.py
+++ bleak/backends/p4android/defs.py
@@ -36,12 +36,12 @@ SCAN_FAILED_NAMES = {
 }
 
 GATT_SUCCESS = 0x0000
+# TODO: we may need different lookups, e.g. one for bleak.exc.CONTROLLER_ERROR_CODES
 GATT_STATUS_STRINGS = {
     # https://developer.android.com/reference/android/bluetooth/BluetoothGatt
     # https://android.googlesource.com/platform/external/bluetooth/bluedroid/+/5738f83aeb59361a0a2eda2460113f6dc9194271/stack/include/gatt_api.h
     # https://android.googlesource.com/platform/system/bt/+/master/stack/include/gatt_api.h
     # https://www.bluetooth.com/specifications/bluetooth-core-specification/
-    **bleak.exc.CONTROLLER_ERROR_CODES,
     **bleak.exc.PROTOCOL_ERROR_CODES,
     0x007F: "Too Short",
     0x0080: "No Resources",
