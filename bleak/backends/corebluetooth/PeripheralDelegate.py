hbldh/bleak

commit ad59f6e8e35cbab8276d654faafa7856c1fd8b9b

CoreBluetooth: delete futures after awaiting
In the CoreBluetooth backend, futures are used to pass results/errors
from the delegate callbacks to methods that are waiting for them. The
futures are stored as part of the object state so that the delegates
can access them.

Prior to this change, completed futures were not removed from the
object status. This worked most of the time because a second call
of the same method would replace the completed future from the prior
call. However, in the case of notifications and reads, the same
delegate callback is shared with two methods. So when both of these
were used, notifications would attempt to complete the already completed
read future which raises an InvalidStateError.

Fixes #675.

bleak/backends/corebluetooth/PeripheralDelegate.py
@@ -87,29 +87,43 @@ class PeripheralDelegate(NSObject):
     @objc.python_method
     async def discover_services(self) -> NSArray:
         future = self._event_loop.create_future()
+
         self._services_discovered_future = future
-        self.peripheral.discoverServices_(None)
-        await future
+        try:
+            self.peripheral.discoverServices_(None)
+            await future
+        finally:
+            del self._services_discovered_future
 
         return self.peripheral.services()
 
     @objc.python_method
     async def discover_characteristics(self, service: CBService) -> NSArray:
         future = self._event_loop.create_future()
+
         self._service_characteristic_discovered_futures[service.startHandle()] = future
-        self.peripheral.discoverCharacteristics_forService_(None, service)
-        await future
+        try:
+            self.peripheral.discoverCharacteristics_forService_(None, service)
+            await future
+        finally:
+            del self._service_characteristic_discovered_futures[service.startHandle()]
 
         return service.characteristics()
 
     @objc.python_method
     async def discover_descriptors(self, characteristic: CBCharacteristic) -> NSArray:
         future = self._event_loop.create_future()
+
         self._characteristic_descriptor_discover_futures[
             characteristic.handle()
         ] = future
-        self.peripheral.discoverDescriptorsForCharacteristic_(characteristic)
-        await future
+        try:
+            self.peripheral.discoverDescriptorsForCharacteristic_(characteristic)
+            await future
+        finally:
+            del self._characteristic_descriptor_discover_futures[
+                characteristic.handle()
+            ]
 
         return characteristic.descriptors()
 
@@ -121,9 +135,14 @@ class PeripheralDelegate(NSObject):
             return characteristic.value()
 
         future = self._event_loop.create_future()
+
         self._characteristic_read_futures[characteristic.handle()] = future
-        self.peripheral.readValueForCharacteristic_(characteristic)
-        await asyncio.wait_for(future, timeout=5)
+        try:
+            self.peripheral.readValueForCharacteristic_(characteristic)
+            await asyncio.wait_for(future, timeout=5)
+        finally:
+            del self._characteristic_read_futures[characteristic.handle()]
+
         if characteristic.value():
             return characteristic.value()
         else:
@@ -137,9 +156,13 @@ class PeripheralDelegate(NSObject):
             return descriptor.value()
 
         future = self._event_loop.create_future()
+
         self._descriptor_read_futures[descriptor.handle()] = future
-        self.peripheral.readValueForDescriptor_(descriptor)
-        await future
+        try:
+            self.peripheral.readValueForDescriptor_(descriptor)
+            await future
+        finally:
+            del self._descriptor_read_futures[descriptor.handle()]
 
         return descriptor.value()
 
@@ -154,23 +177,32 @@ class PeripheralDelegate(NSObject):
         # CBCharacteristicWriteWithoutResponse
         if response == CBCharacteristicWriteWithResponse:
             future = self._event_loop.create_future()
+
             self._characteristic_write_futures[characteristic.handle()] = future
-
-        self.peripheral.writeValue_forCharacteristic_type_(
-            value, characteristic, response
-        )
-
-        if response == CBCharacteristicWriteWithResponse:
-            await future
+            try:
+                self.peripheral.writeValue_forCharacteristic_type_(
+                    value, characteristic, response
+                )
+                await future
+            finally:
+                del self._characteristic_write_futures[characteristic.handle()]
+        else:
+            self.peripheral.writeValue_forCharacteristic_type_(
+                value, characteristic, response
+            )
 
         return True
 
     @objc.python_method
     async def write_descriptor(self, descriptor: CBDescriptor, value: NSData) -> bool:
         future = self._event_loop.create_future()
+
         self._descriptor_write_futures[descriptor.handle()] = future
-        self.peripheral.writeValue_forDescriptor_(value, descriptor)
-        await future
+        try:
+            self.peripheral.writeValue_forDescriptor_(value, descriptor)
+            await future
+        finally:
+            del self._descriptor_write_futures[descriptor.handle()]
 
         return True
 
@@ -185,9 +217,13 @@ class PeripheralDelegate(NSObject):
         self._characteristic_notify_callbacks[c_handle] = callback
 
         future = self._event_loop.create_future()
+
         self._characteristic_notify_change_futures[c_handle] = future
-        self.peripheral.setNotifyValue_forCharacteristic_(True, characteristic)
-        await future
+        try:
+            self.peripheral.setNotifyValue_forCharacteristic_(True, characteristic)
+            await future
+        finally:
+            del self._characteristic_notify_change_futures[c_handle]
 
         return True
 
@@ -198,9 +234,13 @@ class PeripheralDelegate(NSObject):
             raise ValueError("Characteristic notification never started")
 
         future = self._event_loop.create_future()
+
         self._characteristic_notify_change_futures[c_handle] = future
-        self.peripheral.setNotifyValue_forCharacteristic_(False, characteristic)
-        await future
+        try:
+            self.peripheral.setNotifyValue_forCharacteristic_(False, characteristic)
+            await future
+        finally:
+            del self._characteristic_notify_change_futures[c_handle]
 
         self._characteristic_notify_callbacks.pop(c_handle)
 
@@ -209,9 +249,13 @@ class PeripheralDelegate(NSObject):
     @objc.python_method
     async def read_rssi(self) -> NSNumber:
         future = self._event_loop.create_future()
+
         self._read_rssi_futures[self.peripheral.identifier()] = future
-        self.peripheral.readRSSI()
-        return await future
+        try:
+            self.peripheral.readRSSI()
+            return await future
+        finally:
+            del self._read_rssi_futures[self.peripheral.identifier()]
 
     # Protocol Functions
 
@@ -388,7 +432,7 @@ class PeripheralDelegate(NSObject):
         characteristic: CBCharacteristic,
         error: Optional[NSError],
     ):
-        future = self._characteristic_write_futures.pop(characteristic.handle(), None)
+        future = self._characteristic_write_futures.get(characteristic.handle(), None)
         if not future:
             return  # event only expected on write with response
         if error is not None:
@@ -489,7 +533,7 @@ class PeripheralDelegate(NSObject):
     def did_read_rssi(
         self, peripheral: CBPeripheral, rssi: NSNumber, error: Optional[NSError]
     ) -> None:
-        future = self._read_rssi_futures.pop(peripheral.identifier(), None)
+        future = self._read_rssi_futures.get(peripheral.identifier(), None)
 
         if not future:
             logger.warning("Unexpected event did_read_rssi")

bleak/backends/corebluetooth/CentralManagerDelegate.py
@@ -152,25 +152,38 @@ class CentralManagerDelegate(NSObject):
         try:
             self._disconnect_callbacks[peripheral.identifier()] = disconnect_callback
             future = self.event_loop.create_future()
+
             self._connect_futures[peripheral.identifier()] = future
-            self.central_manager.connectPeripheral_options_(peripheral, None)
-            await asyncio.wait_for(future, timeout=timeout)
+            try:
+                self.central_manager.connectPeripheral_options_(peripheral, None)
+                await asyncio.wait_for(future, timeout=timeout)
+            finally:
+                del self._connect_futures[peripheral.identifier()]
+
         except asyncio.TimeoutError:
             logger.debug(f"Connection timed out after {timeout} seconds.")
             del self._disconnect_callbacks[peripheral.identifier()]
             future = self.event_loop.create_future()
+
             self._disconnect_futures[peripheral.identifier()] = future
-            self.central_manager.cancelPeripheralConnection_(peripheral)
-            await future
+            try:
+                self.central_manager.cancelPeripheralConnection_(peripheral)
+                await future
+            finally:
+                del self._disconnect_futures[peripheral.identifier()]
+
             raise
 
     @objc.python_method
     async def disconnect(self, peripheral: CBPeripheral) -> None:
         future = self.event_loop.create_future()
+
         self._disconnect_futures[peripheral.identifier()] = future
-        self.central_manager.cancelPeripheralConnection_(peripheral)
-        await future
-        del self._disconnect_callbacks[peripheral.identifier()]
+        try:
+            self.central_manager.cancelPeripheralConnection_(peripheral)
+            await future
+        finally:
+            del self._disconnect_callbacks[peripheral.identifier()]
 
     @objc.python_method
     def _changed_is_scanning(self, is_scanning: bool) -> None:
@@ -280,7 +293,7 @@ class CentralManagerDelegate(NSObject):
     def did_connect_peripheral(
         self, central: CBCentralManager, peripheral: CBPeripheral
     ) -> None:
-        future = self._connect_futures.pop(peripheral.identifier(), None)
+        future = self._connect_futures.get(peripheral.identifier(), None)
         if future is not None:
             future.set_result(True)
 
@@ -301,7 +314,7 @@ class CentralManagerDelegate(NSObject):
         peripheral: CBPeripheral,
         error: Optional[NSError],
     ) -> None:
-        future = self._connect_futures.pop(peripheral.identifier(), None)
+        future = self._connect_futures.get(peripheral.identifier(), None)
         if future is not None:
             if error is not None:
                 future.set_exception(BleakError(f"failed to connect: {error}"))
@@ -331,7 +344,7 @@ class CentralManagerDelegate(NSObject):
     ) -> None:
         logger.debug("Peripheral Device disconnected!")
 
-        future = self._disconnect_futures.pop(peripheral.identifier(), None)
+        future = self._disconnect_futures.get(peripheral.identifier(), None)
         if future is not None:
             if error is not None:
                 future.set_exception(BleakError(f"disconnect failed: {error}"))

commit e893611ac4e1f43cc1253f9bd775a1a9cef55d5d

corebluetooth: fix crash on disconnect
Fixes the following exception:

    Traceback (most recent call last):
    File "/Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/asyncio/events.py", line 80, in _run
        self._context.run(self._callback, *self._args)
    File "/Users/david/work/bleak/bleak/backends/corebluetooth/CentralManagerDelegate.py", line 358, in did_disconnect_peripheral
        callback()
    File "/Users/david/work/bleak/bleak/backends/corebluetooth/client.py", line 105, in disconnect_callback
        for future in self._delegate.futures():
    File "/Users/david/work/bleak/bleak/backends/corebluetooth/PeripheralDelegate.py", line 77, in futures
        if self._services_discovered_future
    AttributeError: 'PeripheralDelegate' object has no attribute '_services_discovered_future'

Problem was introduced by commit ad59f6e8e35cbab8276d654faafa7856c1fd8b9b.

bleak/backends/corebluetooth/PeripheralDelegate.py
@@ -72,8 +72,14 @@ class PeripheralDelegate(NSObject):
 
         These can be used to handle any pending futures when a peripheral is disconnected.
         """
+        services_discovered_future = (
+            (self._services_discovered_future,)
+            if hasattr(self, "_services_discovered_future")
+            else ()
+        )
+
         return itertools.chain(
-            (self._services_discovered_future,),
+            services_discovered_future,
             self._service_characteristic_discovered_futures.values(),
             self._characteristic_descriptor_discover_futures.values(),
             self._characteristic_read_futures.values(),