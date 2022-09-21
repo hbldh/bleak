hbldh/bleak

commit 0150ef30a2e357a43e150474798b5ace9c4f8988

add concrete BleakScanner and BleakClient classes
This allows us to put all of the documentation, including platform-
specific quirks, for BleakScanner and BleakClient in one place. Docs
for these classes are move to new sub-pages of the API docs page and
the backend docs are moved to the backend pages of the docs.

It will also allow us to eventually eliminate quite a bit of duplicated
code from the backends by moving it to these shared classes.

Several methods that provided duplicate functionality have been
deprecated (previously they were required because of backend
implementation details but can now be safely removed from the
top-level classes).

bleak/backends/winrt/client.py
@@ -162,7 +162,7 @@ class BleakClientWinRT(BaseBleakClient):
         self,
         address_or_ble_device: Union[BLEDevice, str],
         *,
-        winrt: WinRTClientArgs = {},
+        winrt: WinRTClientArgs,
         **kwargs,
     ):
         super(BleakClientWinRT, self).__init__(address_or_ble_device, **kwargs)

bleak/backends/client.py
@@ -7,10 +7,13 @@ Created on 2018-04-23 by hbldh <henrik.blidh@nedomkull.com>
 """
 import abc
 import asyncio
+import os
+import platform
 import uuid
-from typing import Callable, Optional, Union
+from typing import Callable, Optional, Type, Union
 from warnings import warn
 
+from ..exc import BleakError
 from .service import BleakGATTServiceCollection
 from .characteristic import BleakGATTCharacteristic
 from .device import BLEDevice
@@ -44,25 +47,6 @@ class BaseBleakClient(abc.ABC):
 
         self._timeout = kwargs.get("timeout", 10.0)
         self._disconnected_callback = kwargs.get("disconnected_callback")
-
-    def __str__(self):
-        return "{0}, {1}".format(self.__class__.__name__, self.address)
-
-    def __repr__(self):
-        return "<{0}, {1}, {2}>".format(
-            self.__class__.__name__,
-            self.address,
-            super(BaseBleakClient, self).__repr__(),
-        )
-
-    # Async Context managers
-
-    async def __aenter__(self):
-        await self.connect()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.disconnect()
 
     # Connectivity methods
 
@@ -171,7 +155,7 @@ class BaseBleakClient(abc.ABC):
     async def read_gatt_char(
         self,
         char_specifier: Union[BleakGATTCharacteristic, int, str, uuid.UUID],
-        **kwargs
+        **kwargs,
     ) -> bytearray:
         """Perform read operation on the specified GATT characteristic.
 
@@ -236,7 +220,7 @@ class BaseBleakClient(abc.ABC):
         self,
         char_specifier: Union[BleakGATTCharacteristic, int, str, uuid.UUID],
         callback: Callable[[int, bytearray], None],
-        **kwargs
+        **kwargs,
     ) -> None:
         """Activate notifications/indications on a characteristic.
 
@@ -271,3 +255,30 @@ class BaseBleakClient(abc.ABC):
 
         """
         raise NotImplementedError()
+
+
+def get_platform_client_backend_type() -> Type[BaseBleakClient]:
+    """
+    Gets the platform-specific :class:`BaseBleakClient` type.
+    """
+    if os.environ.get("P4A_BOOTSTRAP") is not None:
+        from bleak.backends.p4android.client import BleakClientP4Android
+
+        return BleakClientP4Android
+
+    if platform.system() == "Linux":
+        from bleak.backends.bluezdbus.client import BleakClientBlueZDBus
+
+        return BleakClientBlueZDBus
+
+    if platform.system() == "Darwin":
+        from bleak.backends.corebluetooth.client import BleakClientCoreBluetooth
+
+        return BleakClientCoreBluetooth
+
+    if platform.system() == "Windows":
+        from bleak.backends.winrt.client import BleakClientWinRT
+
+        return BleakClientWinRT
+
+    raise BleakError(f"Unsupported platform: {platform.system()}")

.github/workflows/format_and_lint.yml
@@ -21,4 +21,4 @@ jobs:
             -   name: Lint with flake8
                 run: pipx run poetry run flake8 . --count --show-source --statistics
             -   name: Build docs
-                run: READTHEDOCS=True pipx run poetry run make -C docs html
+                run: pipx run poetry run make -C docs html

bleak/backends/bluezdbus/scanner.py
@@ -75,11 +75,11 @@ class BleakScannerBlueZDBus(BaseBleakScanner):
 
     def __init__(
         self,
-        detection_callback: Optional[AdvertisementDataCallback] = None,
-        service_uuids: Optional[List[str]] = None,
-        scanning_mode: Literal["active", "passive"] = "active",
+        detection_callback: Optional[AdvertisementDataCallback],
+        service_uuids: Optional[List[str]],
+        scanning_mode: Literal["active", "passive"],
         *,
-        bluez: BlueZScannerArgs = {},
+        bluez: BlueZScannerArgs,
         **kwargs,
     ):
         super(BleakScannerBlueZDBus, self).__init__(detection_callback, service_uuids)

bleak/backends/corebluetooth/scanner.py
@@ -50,9 +50,9 @@ class BleakScannerCoreBluetooth(BaseBleakScanner):
 
     def __init__(
         self,
-        detection_callback: Optional[AdvertisementDataCallback] = None,
-        service_uuids: Optional[List[str]] = None,
-        scanning_mode: Literal["active", "passive"] = "active",
+        detection_callback: Optional[AdvertisementDataCallback],
+        service_uuids: Optional[List[str]],
+        scanning_mode: Literal["active", "passive"],
         **kwargs
     ):
         super(BleakScannerCoreBluetooth, self).__init__(

bleak/backends/p4android/scanner.py
@@ -44,9 +44,9 @@ class BleakScannerP4Android(BaseBleakScanner):
 
     def __init__(
         self,
-        detection_callback: Optional[AdvertisementDataCallback] = None,
-        service_uuids: Optional[List[str]] = None,
-        scanning_mode: Literal["active", "passive"] = "active",
+        detection_callback: Optional[AdvertisementDataCallback],
+        service_uuids: Optional[List[str]],
+        scanning_mode: Literal["active", "passive"],
         **kwargs,
     ):
         super(BleakScannerP4Android, self).__init__(detection_callback, service_uuids)

bleak/backends/scanner.py
@@ -1,11 +1,11 @@
 import abc
 import asyncio
 import inspect
-from typing import Awaitable, Callable, Dict, List, Optional, Tuple
-from warnings import warn
+import os
+import platform
+from typing import Awaitable, Callable, Dict, List, Optional, Tuple, Type
 
-import async_timeout
-
+from ..exc import BleakError
 from .device import BLEDevice
 
 
@@ -93,32 +93,6 @@ class BaseBleakScanner(abc.ABC):
             [u.lower() for u in service_uuids] if service_uuids is not None else None
         )
 
-    async def __aenter__(self):
-        await self.start()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.stop()
-
-    @classmethod
-    async def discover(cls, timeout=5.0, **kwargs) -> List[BLEDevice]:
-        """Scan continuously for ``timeout`` seconds and return discovered devices.
-
-        Args:
-            timeout: Time to scan for.
-
-        Keyword Args:
-            **kwargs: Implementations might offer additional keyword arguments sent to the constructor of the
-                      BleakScanner class.
-
-        Returns:
-
-        """
-        async with cls(**kwargs) as scanner:
-            await asyncio.sleep(timeout)
-            devices = scanner.discovered_devices
-        return devices
-
     def register_detection_callback(
         self, callback: Optional[AdvertisementDataCallback]
     ) -> None:
@@ -183,74 +157,29 @@ class BaseBleakScanner(abc.ABC):
         """
         raise NotImplementedError()
 
-    async def get_discovered_devices(self) -> List[BLEDevice]:
-        """Gets the devices registered by the BleakScanner.
 
-        .. deprecated:: 0.11.0
-            This method will be removed in a future version of Bleak. Use the
-            :attr:`.discovered_devices` property instead.
+def get_platform_scanner_backend_type() -> Type[BaseBleakScanner]:
+    """
+    Gets the platform-specific :class:`BaseBleakScanner` type.
+    """
+    if os.environ.get("P4A_BOOTSTRAP") is not None:
+        from bleak.backends.p4android.scanner import BleakScannerP4Android
 
-        Returns:
-            A list of the devices that the scanner has discovered during the scanning.
+        return BleakScannerP4Android
 
-        """
-        warn(
-            "This method will be removed in a future version of Bleak. Use the `discovered_devices` property instead.",
-            FutureWarning,
-            stacklevel=2,
-        )
-        return self.discovered_devices
+    if platform.system() == "Linux":
+        from bleak.backends.bluezdbus.scanner import BleakScannerBlueZDBus
 
-    @classmethod
-    async def find_device_by_address(
-        cls, device_identifier: str, timeout: float = 10.0, **kwargs
-    ) -> Optional[BLEDevice]:
-        """A convenience method for obtaining a ``BLEDevice`` object specified by Bluetooth address or (macOS) UUID address.
+        return BleakScannerBlueZDBus
 
-        Args:
-            device_identifier (str): The Bluetooth/UUID address of the Bluetooth peripheral sought.
-            timeout (float): Optional timeout to wait for detection of specified peripheral before giving up. Defaults to 10.0 seconds.
+    if platform.system() == "Darwin":
+        from bleak.backends.corebluetooth.scanner import BleakScannerCoreBluetooth
 
-        Keyword Args:
-            adapter (str): Bluetooth adapter to use for discovery.
+        return BleakScannerCoreBluetooth
 
-        Returns:
-            The ``BLEDevice`` sought or ``None`` if not detected.
+    if platform.system() == "Windows":
+        from bleak.backends.winrt.scanner import BleakScannerWinRT
 
-        """
-        device_identifier = device_identifier.lower()
-        return await cls.find_device_by_filter(
-            lambda d, ad: d.address.lower() == device_identifier,
-            timeout=timeout,
-            **kwargs,
-        )
+        return BleakScannerWinRT
 
-    @classmethod
-    async def find_device_by_filter(
-        cls, filterfunc: AdvertisementDataFilter, timeout: float = 10.0, **kwargs
-    ) -> Optional[BLEDevice]:
-        """A convenience method for obtaining a ``BLEDevice`` object specified by a filter function.
-
-        Args:
-            filterfunc (AdvertisementDataFilter): A function that is called for every BLEDevice found. It should return True only for the wanted device.
-            timeout (float): Optional timeout to wait for detection of specified peripheral before giving up. Defaults to 10.0 seconds.
-
-        Keyword Args:
-            adapter (str): Bluetooth adapter to use for discovery.
-
-        Returns:
-            The ``BLEDevice`` sought or ``None`` if not detected.
-
-        """
-        found_device_queue = asyncio.Queue()
-
-        def apply_filter(d: BLEDevice, ad: AdvertisementData):
-            if filterfunc(d, ad):
-                found_device_queue.put_nowait(d)
-
-        async with cls(detection_callback=apply_filter, **kwargs):
-            try:
-                async with async_timeout.timeout(timeout):
-                    return await found_device_queue.get()
-            except asyncio.TimeoutError:
-                return None
+    raise BleakError(f"Unsupported platform: {platform.system()}")

commit 4bba1996702071c6419635827088ec54869e6517

backends: fix implicit scanning when connecting
0150ef3 broke implicit scanning because some methods were moved from
the backends to the new top-level class.

This fixes the issue by using the new backends kwarg of the scanner
to pass the matching scanner backend to the client. This also revealed
a bug in the backend kwarg implementation that is now fixed.

bleak/__init__.py
@@ -86,13 +86,13 @@ class BleakScanner:
         backend: Optional[BaseBleakScanner] = None,
         **kwargs,
     ):
-        if backend is None:
-            PlatformBleakScanner = get_platform_scanner_backend_type()
-            self._backend = PlatformBleakScanner(
-                detection_callback, service_uuids, scanning_mode, bluez=bluez, **kwargs
-            )
-        else:
-            self._backend = backend
+        PlatformBleakScanner = (
+            get_platform_scanner_backend_type() if backend is None else backend
+        )
+
+        self._backend = PlatformBleakScanner(
+            detection_callback, service_uuids, scanning_mode, bluez=bluez, **kwargs
+        )
 
     async def __aenter__(self):
         await self._backend.start()
@@ -305,17 +305,17 @@ class BleakClient:
         backend: Optional[BaseBleakClient] = None,
         **kwargs,
     ):
-        if backend is None:
-            PlatformBleakClient = get_platform_client_backend_type()
-            self._backend = PlatformBleakClient(
-                device_or_address,
-                disconnected_callback=disconnected_callback,
-                timeout=timeout,
-                winrt=winrt,
-                **kwargs,
-            )
-        else:
-            self._backend = backend
+        PlatformBleakClient = (
+            get_platform_client_backend_type() if backend is None else backend
+        )
+
+        self._backend = PlatformBleakClient(
+            device_or_address,
+            disconnected_callback=disconnected_callback,
+            timeout=timeout,
+            winrt=winrt,
+            **kwargs,
+        )
 
     # device info
 

bleak/backends/bluezdbus/client.py
@@ -16,6 +16,7 @@ from dbus_fast.constants import BusType, ErrorType
 from dbus_fast.message import Message
 from dbus_fast.signature import Variant
 
+from ... import BleakScanner
 from ...exc import BleakDBusError, BleakError
 from ..client import BaseBleakClient
 from ..device import BLEDevice
@@ -102,8 +103,11 @@ class BleakClientBlueZDBus(BaseBleakClient):
         # Find the desired device before trying to connect.
         timeout = kwargs.get("timeout", self._timeout)
         if self._device_path is None:
-            device = await BleakScannerBlueZDBus.find_device_by_address(
-                self.address, timeout=timeout, adapter=self._adapter
+            device = await BleakScanner.find_device_by_address(
+                self.address,
+                timeout=timeout,
+                adapter=self._adapter,
+                backend=BleakScannerBlueZDBus,
             )
 
             if device:

bleak/backends/corebluetooth/client.py
@@ -17,6 +17,7 @@ from CoreBluetooth import (
 )
 from Foundation import NSArray, NSData
 
+from ... import BleakScanner
 from ...exc import BleakError
 from ..characteristic import BleakGATTCharacteristic
 from ..client import BaseBleakClient
@@ -72,8 +73,8 @@ class BleakClientCoreBluetooth(BaseBleakClient):
         """
         timeout = kwargs.get("timeout", self._timeout)
         if self._peripheral is None:
-            device = await BleakScannerCoreBluetooth.find_device_by_address(
-                self.address, timeout=timeout
+            device = await BleakScanner.find_device_by_address(
+                self.address, timeout=timeout, backend=BleakScannerCoreBluetooth
             )
 
             if device:

bleak/backends/winrt/client.py
@@ -48,6 +48,7 @@ from bleak_winrt.windows.devices.enumeration import (
 from bleak_winrt.windows.foundation import EventRegistrationToken
 from bleak_winrt.windows.storage.streams import Buffer
 
+from ... import BleakScanner
 from ...exc import PROTOCOL_ERROR_CODES, BleakError
 from ..characteristic import BleakGATTCharacteristic
 from ..client import BaseBleakClient
@@ -221,8 +222,8 @@ class BleakClientWinRT(BaseBleakClient):
         # Try to find the desired device.
         timeout = kwargs.get("timeout", self._timeout)
         if self._device_info is None:
-            device = await BleakScannerWinRT.find_device_by_address(
-                self.address, timeout=timeout
+            device = await BleakScanner.find_device_by_address(
+                self.address, timeout=timeout, backend=BleakScannerWinRT
             )
 
             if device: