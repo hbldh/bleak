8204742e272a3d094f2d6d9db8cb8eea76391038
Al Little <alisdair.little@gmail.com>
2023-05-25 10:05:10 +0100

    Added test to check longer UUID's

--- tests/test_uuid.py
+++ tests/test_uuid.py
@@ -3,6 +3,7 @@ from bleak.uuids import normalize_uuid_str
 
 def test_uuid_length_normalization():
     assert normalize_uuid_str("1801") == "00001801-0000-1000-8000-00805f9b34fb"
+    assert normalize_uuid_str("DAF51C01") == "daf51c01-0000-1000-8000-00805f9b34fb"
 
 
 def test_uuid_case_normalization():
