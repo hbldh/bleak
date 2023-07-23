8a720ab835932c5a4f423fee157b300ca6021a6e
Bojan Potočnik <info@bojanpotocnik.com>
2023-07-23 22:19:30 +0200

    BleakScanner: Add async iterator scanning capability (#1361)
    Add `advertisement_data()` async iterator method to the `BleakScanner`
    which yields results of the ongoing scan.

---
+++ examples/scan_iterator.py
@@ -0,0 +1,37 @@
+"""
+Scan/Discovery Async Iterator
+--------------
+
+Example showing how to scan for BLE devices using async iterator instead of callback function
+
+Created on 2023-07-07 by bojanpotocnik <info@bojanpotocnik.com>
+
+"""
+import asyncio
+
+from bleak import BleakScanner
+
+
+async def main():
+    async with BleakScanner() as scanner:
+        print("Scanning...")
+
+        n = 5
+        print(f"\n{n} advertisement packets:")
+        async for bd, ad in scanner.advertisement_data():
+            print(f" {n}. {bd!r} with {ad!r}")
+            n -= 1
+            if n == 0:
+                break
+
+        n = 10
+        print(f"\nFind device with name longer than {n} characters...")
+        async for bd, ad in scanner.advertisement_data():
+            found = len(bd.name or "") > n or len(ad.local_name or "") > n
+            print(f" Found{' it' if found else ''} {bd!r} with {ad!r}")
+            if found:
+                break
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
