08bc8810962a0a5b53e5d81ec7e326f2dd1d8633
J. Nick Koston <nick@koston.org>
2023-08-28 11:36:04 -0500

    fix tests

--- tests/bleak/backends/bluezdbus/test_utils.py
+++ tests/bleak/backends/bluezdbus/test_utils.py
@@ -2,11 +2,19 @@
 
 """Tests for `bleak.backends.bluezdbus.utils` package."""
 
-from bleak.backends.bluezdbus.utils import device_path_from_characteristic_path
+import pytest
+import sys
 
 
+@pytest.mark.skipif(
+    not sys.platform.startswith("linux"), reason="requires dbus-fast on Linux"
+)
 def test_device_path_from_characteristic_path():
     """Test device_path_from_characteristic_path."""
+    from bleak.backends.bluezdbus.utils import (  # pylint: disable=import-outside-toplevel
+        device_path_from_characteristic_path,
+    )
+
     assert (
         device_path_from_characteristic_path(
             "/org/bluez/hci0/dev_11_22_33_44_55_66/service000c/char000d"
