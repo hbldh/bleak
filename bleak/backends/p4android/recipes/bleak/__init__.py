e8050d0cc5d59c902f567973eeb8cfed6427f545
roga <robbegaeremynck@gmail.com>
2023-08-28 21:59:50 +0200

    Removing unnecessary parts in fix_setup.py + code formatting with black

--- bleak/backends/p4android/recipes/bleak/__init__.py
+++ bleak/backends/p4android/recipes/bleak/__init__.py
@@ -8,34 +8,34 @@ from os.path import join
 
 class BleakRecipe(PythonRecipe):
     version = None  # Must be none for p4a to correctly clone repo
-    fix_setup_py_version = 'bleak develop branch'
-    url = 'git+https://github.com/hbldh/bleak.git'
+    fix_setup_py_version = "bleak develop branch"
+    url = "git+https://github.com/hbldh/bleak.git"
     name = "bleak"
 
     depends = ["pyjnius"]
     call_hostpython_via_targetpython = False
 
-    fix_setup_filename = 'fix_setup.py'
+    fix_setup_filename = "fix_setup.py"
 
     def prepare_build_dir(self, arch):
         super().prepare_build_dir(arch)  # Unpack the url file to the get_build_dir
         build_dir = self.get_build_dir(arch)
 
-        setup_py_path = join(build_dir, 'setup.py')
+        setup_py_path = join(build_dir, "setup.py")
         if not os.path.exists(setup_py_path):
             # Perform the p4a temporary fix
             # At the moment, p4a recipe installing requires setup.py to be present
             # So, we create a setup.py file only for android
 
             fix_setup_py_path = join(self.get_recipe_dir(), self.fix_setup_filename)
-            with open(fix_setup_py_path, 'r') as f:
+            with open(fix_setup_py_path, "r") as f:
                 contents = f.read()
 
             # Write to the correct location and fill in the version number
-            with open(setup_py_path, 'w') as f:
-                f.write(contents.replace('[VERSION]', self.fix_setup_py_version))
+            with open(setup_py_path, "w") as f:
+                f.write(contents.replace("[VERSION]", self.fix_setup_py_version))
         else:
-            info('setup.py found in bleak directory, are you installing older version?')
+            info("setup.py found in bleak directory, are you installing older version?")
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super().get_recipe_env(arch, with_flags_in_cc)
@@ -48,7 +48,9 @@ class BleakRecipe(PythonRecipe):
 
         info("Copying java files")
         dest_dir = self.ctx.javaclass_dir
-        path = join(self.get_build_dir(arch.arch), "bleak", "backends", "p4android", "java", ".")
+        path = join(
+            self.get_build_dir(arch.arch), "bleak", "backends", "p4android", "java", "."
+        )
 
         shprint(sh.cp, "-a", path, dest_dir)
 
