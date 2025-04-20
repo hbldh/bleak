534b58a208c04942b366858d33b66b891ef61a6a
David Lechner <david@lechnology.com>
2025-04-19 23:15:07 -0500

    docs: ignore .venv dircectory
    It is convenient to create a separate virtual environment for the docs.
    However, docs builds can fail because it would otherwise recurse into
    this directory.

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ version = ".".join(release.split(".")[:2])
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
+exclude_patterns = ["_build", ".venv"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
