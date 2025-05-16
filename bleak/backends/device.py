fbc08a90250f341b517ee11727ad5a82f08af347
David Lechner <david@lechnology.com>
2025-05-15 21:02:17 -0500

    Remove deprecated properties from BLEDevice.
    These have been deprecated for 3 years so can be removed.

--- bleak/backends/device.py
+++ bleak/backends/device.py
@@ -9,7 +9,6 @@ Created on 2018-04-23 by hbldh <henrik.blidh@nedomkull.com>
 
 
 from typing import Any, Optional
-from warnings import warn
 
 
 class BLEDevice:
@@ -17,11 +16,9 @@ class BLEDevice:
     A simple wrapper class representing a BLE server detected during scanning.
     """
 
-    __slots__ = ("address", "name", "details", "_rssi", "_metadata")
+    __slots__ = ("address", "name", "details")
 
-    def __init__(
-        self, address: str, name: Optional[str], details: Any, rssi: int, **kwargs
-    ):
+    def __init__(self, address: str, name: Optional[str], details: Any):
         #: The Bluetooth address of the device on this machine (UUID on macOS).
         self.address = address
         #: The operating system name of the device (not necessarily the local name
@@ -29,42 +26,6 @@ class BLEDevice:
         self.name = name
         #: The OS native details required for connecting to the device.
         self.details = details
-
-        # for backwards compatibility
-        self._rssi = rssi
-        self._metadata = kwargs
-
-    @property
-    def rssi(self) -> int:
-        """
-        Gets the RSSI of the last received advertisement.
-
-        .. deprecated:: 0.19.0
-            Use :class:`AdvertisementData` from detection callback or
-            :attr:`BleakScanner.discovered_devices_and_advertisement_data` instead.
-        """
-        warn(
-            "BLEDevice.rssi is deprecated and will be removed in a future version of Bleak, use AdvertisementData.rssi instead",
-            FutureWarning,
-            stacklevel=2,
-        )
-        return self._rssi
-
-    @property
-    def metadata(self) -> dict:
-        """
-        Gets additional advertisement data for the device.
-
-        .. deprecated:: 0.19.0
-            Use :class:`AdvertisementData` from detection callback or
-            :attr:`BleakScanner.discovered_devices_and_advertisement_data` instead.
-        """
-        warn(
-            "BLEDevice.metadata is deprecated and will be removed in a future version of Bleak, use AdvertisementData instead",
-            FutureWarning,
-            stacklevel=2,
-        )
-        return self._metadata
 
     def __str__(self):
         return f"{self.address}: {self.name}"
