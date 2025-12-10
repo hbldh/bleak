92283bcc6c4d951bd304f3566d63b900b93a1f19
timrid <6593626+timrid@users.noreply.github.com>
2025-12-10 20:36:16 +0100

    tests: use monkeypatch instead of unittest.mock.patch
    Since we are using PyTest, this is the more natural way to patch
    things in tests and avoids the need for `with` statements.

--- tests/backends/bluezdbus/test_version.py
+++ tests/backends/bluezdbus/test_version.py
@@ -2,7 +2,7 @@
 
 """Tests for `bleak.backends.bluezdbus.version` package."""
 
-from unittest.mock import AsyncMock, Mock, patch
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 
@@ -28,58 +28,57 @@ def reset_bluez_features(monkeypatch: pytest.MonkeyPatch) -> None:
 )
 async def test_bluez_version(
     version: bytes,
+    monkeypatch: pytest.MonkeyPatch,
 ):
     """Test we can determine supported feature from bluetoothctl."""
     mock_proc = Mock(
         wait=AsyncMock(), stdout=Mock(read=AsyncMock(return_value=version))
     )
-    with patch(
+    monkeypatch.setattr(
         "bleak.backends.bluezdbus.version.asyncio.create_subprocess_exec",
         AsyncMock(return_value=mock_proc),
-    ):
-        await BlueZFeatures.check_bluez_version()
+    )
+    await BlueZFeatures.check_bluez_version()
     assert BlueZFeatures.checked_bluez_version is True
 
 
 @pytest.mark.asyncio
-async def test_bluez_version_only_happens_once():
+async def test_bluez_version_only_happens_once(monkeypatch: pytest.MonkeyPatch):
     """Test we can determine supported feature from bluetoothctl."""
     mock_proc = Mock(
         wait=AsyncMock(),
         stdout=Mock(read=AsyncMock(return_value=b"bluetoothctl: 5.46")),
     )
-    with patch(
+    monkeypatch.setattr(
         "bleak.backends.bluezdbus.version.asyncio.create_subprocess_exec",
         AsyncMock(return_value=mock_proc),
-    ):
-        await BlueZFeatures.check_bluez_version()
+    )
+    await BlueZFeatures.check_bluez_version()
 
     assert BlueZFeatures.checked_bluez_version is True
 
-    with patch(
+    monkeypatch.setattr(
         "bleak.backends.bluezdbus.version.asyncio.create_subprocess_exec",
-        side_effect=Exception,
-    ):
-        await BlueZFeatures.check_bluez_version()
+        AsyncMock(side_effect=Exception),
+    )
+    await BlueZFeatures.check_bluez_version()
 
     assert BlueZFeatures.checked_bluez_version is True
 
 
 @pytest.mark.asyncio
-async def test_exception_checking_bluez_features_does_not_block_forever():
+async def test_exception_checking_bluez_features_does_not_block_forever(
+    monkeypatch: pytest.MonkeyPatch,
+):
     """Test an exception while checking BlueZ features does not stall a second check."""
-    with patch(
+    monkeypatch.setattr(
         "bleak.backends.bluezdbus.version.asyncio.create_subprocess_exec",
-        side_effect=OSError,
-    ):
-        await BlueZFeatures.check_bluez_version()
+        AsyncMock(side_effect=OSError),
+    )
+    await BlueZFeatures.check_bluez_version()
 
     assert BlueZFeatures.checked_bluez_version is True
 
-    with patch(
-        "bleak.backends.bluezdbus.version.asyncio.create_subprocess_exec",
-        side_effect=OSError,
-    ):
-        await BlueZFeatures.check_bluez_version()
+    await BlueZFeatures.check_bluez_version()
 
     assert BlueZFeatures.checked_bluez_version is True
