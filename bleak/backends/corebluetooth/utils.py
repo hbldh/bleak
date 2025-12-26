a335917b0a76e649d34b1c648e7b6741da7f65c1
timrid <6593626+timrid@users.noreply.github.com>
2025-12-26 22:44:36 +0100

    backends/corebluetooth: optimize advertising data parsing and type hints.
    This add type hints for advertising data dictionary, optimize parsing of advertising data, convert 'tx_power' explicitly to 'int' because it is a 'NSNumber' and can theoretically be a float and 'local_name' explicitly to 'str' in preparation for rubicon-objc.

--- bleak/backends/corebluetooth/utils.py
+++ bleak/backends/corebluetooth/utils.py
@@ -5,7 +5,10 @@ if TYPE_CHECKING:
     if sys.platform != "darwin":
         assert False, "This backend is only available on macOS"
 
+from typing import Optional, overload
+
 from CoreBluetooth import CBUUID
+from Foundation import NSNumber, NSString
 
 from bleak.uuids import normalize_uuid_str
 
@@ -23,3 +26,45 @@ def cb_uuid_to_str(uuid: CBUUID) -> str:
         The UUID as a lower case Python string (``xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx``)
     """
     return normalize_uuid_str(uuid.UUIDString())
+
+
+@overload
+def to_optional_str(value: NSString) -> str: ...
+@overload
+def to_optional_str(value: None) -> None: ...
+
+
+def to_optional_str(value: Optional[NSString]) -> Optional[str]:
+    """Converts an NSString to a Python string or None.
+
+    Args:
+        value: The NSString or None.
+
+    Returns:
+        The Python string or None.
+    """
+    if value is None:
+        return None
+
+    return str(value)
+
+
+@overload
+def to_optional_int(value: NSNumber) -> int: ...
+@overload
+def to_optional_int(value: None) -> None: ...
+
+
+def to_optional_int(value: Optional[NSNumber]) -> Optional[int]:
+    """Converts an NSNumber to a Python int or None.
+
+    Args:
+        value: The NSNumber or None.
+
+    Returns:
+        The Python int or None.
+    """
+    if value is None:
+        return None
+
+    return int(value)
