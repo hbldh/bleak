ce630e8a1be4425dedd496af243f713794f12b49
Patrick <14628713+patman15@users.noreply.github.com>
2026-03-22 18:01:07 +0100

    moved description to linux client

--- bleak/backends/bluezdbus/client.py
+++ bleak/backends/bluezdbus/client.py
@@ -924,9 +924,9 @@ class BleakClientBlueZDBus(BaseBleakClient):
         Args:
             characteristic (BleakGATTCharacteristic): The characteristic to activate notification/indication on.
             callback (NotifyCallback): The callback to call when a notification/indication is received.
-            use_start_notify (bool, optional): Force using StartNotify instead of AcquireNotify for notifications,
-                even if AcquireNotify is supported by the characteristic. This can be used to work around issues with
-                some devices that don't properly support AcquireNotify. Defaults to False.
+
+        Keyword Args:
+            bluez (dict): dictionary of additional parameters, see :ref:`linux-start-notify` for more details.
         """
 
         bluez: BlueZNotifyArgs = kwargs["bluez"]
